61e50c9ba2831729218bc4bd8db64c785f4228dd
Paul Brandstetter <paulbrandstetter@tuta.io>
2023-01-27 12:57:19 +0100

    Created the pattern database pipeline

---
+++ Assets/Scripts/EnumPattern.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Pattern
+{
+    public Pattern(List<Direction> Sequences, int startIndex)
+    {
+        this.Sequences = Sequences;
+        this.startIndex = startIndex;
+    }
+
+    public List<Direction> Sequences;
+    public int startIndex;
+}
---
+++ Assets/Scripts/PatternDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PatternDatabase", menuName = "Custom/PatternDatabase")]
+public class PatternDatabase : ScriptableObject
+{
+    public List<Pattern> patterns = new List<Pattern>();
+    public Pattern GetRandomPattern()
+    {
+        int randomIndex = Random.Range(0, patterns.Count);
+        return patterns[randomIndex];
+    }
+}
